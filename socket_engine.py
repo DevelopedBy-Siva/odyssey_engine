d0ae38eaab965bc272f3e2479ac8ddb1634cfcb5
DevelopedBy-Siva <developedby.siva@gmail.com>
2025-09-23 18:46:59 -0700

    Refactor the server setup for room creation/join

--- socket_engine.py
+++ socket_engine.py
@@ -18,59 +18,49 @@ class SocketEngine:
     def __connect(self):
         username = request.args.get("username")
         Data.refresh_sid(username, request.sid)
-        print(f"User {username} joined...")
+        print(f"User '{username}' is online...")
 
     def __join_room(self, data):
         username = data["username"]
         room_id = data.get("room")
         option = data.get("option")
 
-        sid = Data.get_sid(username)
         if option == "create":
-            Data.add_user(room_id, username)
-            emit(
-                "join-room",
-                {"status": "SUCCESS", "message": "New room created"},
-                to=sid,
-            )
+            Data.create_room(room_id, username)
+            self.__game_room(username, room_id)
+
         elif option == "join":
             is_join = Data.join_room(room_id, username)
-            if is_join == None:
-                emit(
-                    "join-room",
-                    {"status": "NOT_EXIST", "message": "Room doesn't exist"},
-                    to=sid,
-                )
-            elif not is_join:
-                emit(
-                    "join-room",
-                    {"status": "CANNOT_JOIN", "message": "Cannot join the room"},
-                    to=sid,
-                )
+            if not is_join:
+                self.__notify(msg="Cannot join the room")
             else:
-                emit(
-                    "join-room",
-                    {"status": "JOINED", "message": "Joined the room"},
-                    to=sid,
-                )
-
+                self.__game_room(username, room_id)
         else:
             is_join = Data.join_random_room(room_id, username)
             if is_join:
-                emit(
-                    "join-room",
-                    {"status": "JOINED", "message": "Joined the room"},
-                    to=sid,
-                )
+                self.__game_room(username, room_id)
             else:
-                emit(
-                    "join-room",
-                    {"status": "CANNOT_FIND", "message": "No room free at this time"},
-                    to=sid,
-                )
+                self.__notify(msg="No room free at this time")
 
     def __leave_room(self, data):
         username = data["username"]
         room = data.get("room")
         leave_room(room)
         send(f"{username} has left the room.", to=room)
+
+    def __notify(self, msg):
+        emit("notification", {"message": msg}, to=request.sid)
+
+    def __game_room(self, username, room_id):
+        join_room(room_id)
+        emit(
+            "game-room",
+            {"message": f"'{username}' joined the game. Welcome!"},
+            to=room_id,
+            include_self=False,
+        )
+        emit(
+            "entered-game",
+            {"room_id": room_id},
+            to=request.sid,
+        )
